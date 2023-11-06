afc39e850d30493537d24ced0dd9abdec3a59727
ndivine-eng <d.nubuhoro@alustudent.com>
2023-11-06 08:50:35 -0800

    codes

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx >= len(my_list):
-        return None
-    return my_list[idx]
-
-# Example usage:
-my_list = [1, 2, 3, 4, 5]
-
-# Test cases
-print(element_at(my_list, 2))  # Output: 3
-print(element_at(my_list, -1))  # Output: None
-print(element_at(my_list, 6))  # Output: None
-
+    if idx < 0 or idx > len(my_list) - 1:
+        return 'None'
+    else:
+        return my_list[idx]
