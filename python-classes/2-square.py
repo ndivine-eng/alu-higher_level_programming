b17fea1b81bb4a8c10337e4280fdda0c887f1231
ndivine-eng <138035268+The-divyne@users.noreply.github.com>
2023-11-15 22:51:24 +0200

    updated

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""class that defines square"""
+
+
+class Square:
+    """size is private attribute"""
+    def __init__(self , size=0):
+        """
+        args:
+            size (int): New size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be intiger")
+        elif size < 0:
+            raise ValueError("size must be >=o")
+        self.__size = size
