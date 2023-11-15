d91f2be92e0b30c1ed8ee216be79f64a49280cdb
ndivine-eng <138035268+The-divyne@users.noreply.github.com>
2023-11-15 23:24:06 +0200

    updated

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,13 +4,14 @@
 
 class Square:
     """size is private attribute"""
+
     def __init__(self , size=0):
-        """
+        """ initialize a new square.
         args:
-            size (int): New size
+            size (int): The size of new square.
         """
         if not isinstance(size, int):
-            raise TypeError("size must be an intiger")
+            raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= o")
         self.__size = size
