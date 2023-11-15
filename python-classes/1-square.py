0bc6b6ca30b841f0462a8da500fa62894f5916dc
ndivine-eng <138035268+The-divyne@users.noreply.github.com>
2023-11-15 13:51:02 +0200

    updated

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-description of square.
-"""
+"""description of square."""
 
 
 class Square:
     """This is description of square."""
-
-
     def __init__(self, size):
         self.__size = size
 
