383a7510707904a9d221b52c19c8003838822b06
ndivine-eng <138035268+The-divyne@users.noreply.github.com>
2023-11-16 09:26:18 +0200

    updated

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
-"""access and update private attribute"""
+"""Defne a class square."""
 
 
 class Square:
-    """this is main class"""
+    """Represents a square."""
 
     def __init__(self, size=0):
-        """this is initialisation of square class.
+        """Initialize a new square.
         Args:
-        size (int): The size of new square.
+        size (int): The size of the new square.
         """
         self.size = size
 
-        @property
-        def size(self):
-            """we are setting new current size of the square"""
-            return (self.__size)
+    @property
+    def size(self):
+        """Get/set the current size of the square."""
+        return (self.__size)
 
-        @size.setter
-        def size(self, value):
-            if not isinstance(value, int):
-                raise TypeError("size must be an integer")
-            elif value < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = value
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        def area(self):
-            """Return the current area of the square."""
-            return (self.__size * self.__size)
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
+
+
 
 
 
