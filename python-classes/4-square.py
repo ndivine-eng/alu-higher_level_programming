517c6ccf1bde285381bdffde6c0b0236970b6a18
ndivine-eng <138035268+The-divyne@users.noreply.github.com>
2023-11-16 09:06:08 +0200

    updated

---
+++ python-classes/4-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""access and update private attribute"""
+
+
+class Square:
+    """this is main class"""
+
+    def __init__(self, size=0):
+        """this is initialisation of square class.
+        Args:
+        size (int): The size of new square.
+        """
+        self.size = size
+
+
+
