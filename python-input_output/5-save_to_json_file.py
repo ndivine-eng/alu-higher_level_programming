bec665ddb0964b3d1bdddb27f874c0a36a677cd2
ndivine-eng <138035268+The-divyne@users.noreply.github.com>
2023-11-20 12:28:37 +0200

    updated

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Are you docuemnted ?"""
 
@@ -10,4 +9,3 @@ def save_to_json_file(my_obj, filename):
     """How far now"""
     with open(filename, "w+") as f:
         return json.dump(my_obj, f)
-
