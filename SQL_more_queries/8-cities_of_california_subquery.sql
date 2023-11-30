442f7acb0bc55c89e371d81a2634e9cf4822560f
ndivine-eng <d.nubuhoro@alustudent.com>
2023-11-30 01:34:38 -0800

    genre

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Lists all the cities of california.
-SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "california") ORDER BY ;
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = "california") ORDER BY id;
