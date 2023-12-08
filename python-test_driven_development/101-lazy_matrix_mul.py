86f2e1a28a85d06e0cd91c66f9158b3946e0751b
ndivine-eng <d.nubuhoro@alustudent.com>
2023-12-08 05:29:48 -0800

    commits

--- python-test_driven_development/101-lazy_matrix_mul.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Import multiplication function using NumPy."""
-import numpy as np
-
-
-def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrixes."""
-    return (np.matmul(m_a, m_b))
