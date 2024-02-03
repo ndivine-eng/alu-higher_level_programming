b7c8c9f5a6cd257360bb3767f82bb1e5382e3de5
ndivine-eng <d.nubuhoro@alustudent.com>
2024-02-03 14:10:43 +0200

    err

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -4,7 +4,5 @@ function factorial (n) {
     if (n === 1 || n === 0) return 1;
     return n * factorial(n - 1);
   }
-  
   const num = parseInt(process.argv[2]);
   console.log(factorial(num));
-  
