dad981aecee41f3d47b5ffa493cb55ee2ac2ae29
ndivine-eng <d.nubuhoro@alustudent.com>
2024-02-03 14:08:50 +0200

    cyntax

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const add = (a, b) => {
-const res = Number(a) + Number(b);
-console.log(res);
-};
+  const res = Number(a) + Number(b);
+  console.log(res);
+  };
   
-const first = process.argv[2];
-const second = process.argv[3];
+  const first = process.argv[2];
+  const second = process.argv[3];
   
-add(first, second);
+  add(first, second);
