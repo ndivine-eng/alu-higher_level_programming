a0b8a11390e45e988c3c63814843e503caff0866
ndivine-eng <144015345+ndivine-eng@users.noreply.github.com>
2024-02-04 17:13:47 +0200

    Update 3-rectangle.js

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,16 @@
 #!/usr/bin/node
-const Rectangle = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      let prints = '';
-      for (let xx = 0; xx < this.height; xx++) {
-        for (let yy = 0; yy < this.width; yy++) {
-          prints = prints + 'X';
-        }
-        console.log(prints);
-        prints = '';
-      }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-  };
-  module.exports = Rectangle;
-  
+  }
+}
+module.exports = Rectangle;
