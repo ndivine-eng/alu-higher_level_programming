e07f8f1f1750f87f9206e382acd2ff18b543b62a
ndivine-eng <144015345+ndivine-eng@users.noreply.github.com>
2024-02-04 17:16:11 +0200

    Update 8-esrever.js

--- javascript_objects_scopes_closures/8-esrever.js
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
-    const reversedList = [];
-  
-    for (let i = list.length - 1; i >= 0; i--) {
-      const valueAtIndex = list[i];
-      reversedList.push(valueAtIndex);
-    }
-    return reversedList;
-  };
-  
+  const newList = [];
+  for (let i = 0; i < list.length; i++) {
+    newList.unshift(list[i]);
+  }
+  return newList;
+};
