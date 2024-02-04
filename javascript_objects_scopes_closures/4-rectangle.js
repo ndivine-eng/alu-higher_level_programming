85ea1292fe29ef3070400cc2ac3bfe0d833be3d7
ndivine-eng <144015345+ndivine-eng@users.noreply.github.com>
2024-02-04 17:14:40 +0200

    Update 4-rectangle.js

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,27 @@
 #!/usr/bin/node
-const Rectangle = class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-  
-    print () {
-      let prints = '';
-      for (let xx = 0; xx < this.height; xx++) {
-        for (let yy = 0; yy < this.width; yy++) {
-          prints = prints + 'X';
-        }
-        console.log(prints);
-        prints = '';
-      }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
-  
-    rotate () {
-      const p = this.width;
-      this.width = this.height;
-      this.height = p;
-    }
-  
-    double () {
-      this.width *= 2;
-      this.height *= 2;
-    }
-  };
-  module.exports = Rectangle;
-  
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+module.exports = Rectangle;
